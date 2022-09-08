1c1ae0608ad1bde3fadfbc4707beab7c194d0ce4
GeorgeWahson <georgewahson@qq.com>
2022-09-08 17:28:49 +0800

    添加拦截器

---
+++ src/main/java/com/wahson/config/MPConfig.java
@@ -0,0 +1,20 @@
+package com.wahson.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MPConfig {
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        // TODO 分页拦截器
+
+
+        // 添加乐观锁拦截器
+        mybatisPlusInterceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+        return mybatisPlusInterceptor;
+    }
+}
