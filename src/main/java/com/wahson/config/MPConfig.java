c5e8550547fe2eb999e9cac79f4c8058f7ce6b13
GeorgeWahson <georgewahson@qq.com>
2022-09-10 14:24:37 +0800

    finish

--- src/main/java/com/wahson/config/MPConfig.java
+++ src/main/java/com/wahson/config/MPConfig.java
@@ -6,13 +6,18 @@ import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerIntercept
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * mybatis-plus 配置类
+ * 用于设置拦截器
+ * 分页拦截器 与 乐观锁拦截器
+ */
 @Configuration
 public class MPConfig {
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
-        // TODO 分页拦截器
-//        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        // 分页拦截器
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor());
 
         // 添加乐观锁拦截器
         mybatisPlusInterceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
