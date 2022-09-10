a6426c1022beaf252e769d607b6e7631da1b7a2e
GeorgeWahson <georgewahson@qq.com>
2022-09-10 14:29:03 +0800

    finish

--- src/test/java/com/wahson/InfoSystemApplicationTests.java
+++ src/test/java/com/wahson/InfoSystemApplicationTests.java
@@ -1,13 +1,21 @@
 package com.wahson;
 
+import com.wahson.dao.BrandDao;
+import com.wahson.domain.Brand;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class InfoSystemApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
 
+    @Autowired
+    private BrandDao brandDao;
+
+    @Test
+    void testSelectById() {
+        Brand brand = brandDao.selectById(2);
+        System.out.println(brand);
+    }
 }
