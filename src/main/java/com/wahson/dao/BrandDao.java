56a8ba7cd593236e27dc7c2c7b0ada41fb8d09e6
GeorgeWahson <georgewahson@qq.com>
2022-09-13 14:52:17 +0800

    去除无用import

--- src/main/java/com/wahson/dao/BrandDao.java
+++ src/main/java/com/wahson/dao/BrandDao.java
@@ -3,11 +3,6 @@ package com.wahson.dao;
 import com.wahson.domain.Brand;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.ResultMap;
-import org.apache.ibatis.annotations.Select;
-
-import java.util.List;
 
 /**
  * <p>
