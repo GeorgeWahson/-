2b6937a28d02528c4e0745fd6f55e266b3b83f76
GeorgeWahson <georgewahson@qq.com>
2022-09-13 14:52:45 +0800

    add阻止攻击内部拦截器

--- src/main/java/com/wahson/config/MPConfig.java
+++ src/main/java/com/wahson/config/MPConfig.java
@@ -1,6 +1,7 @@
 package com.wahson.config;
 
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.BlockAttackInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.springframework.context.annotation.Bean;
@@ -9,7 +10,8 @@ import org.springframework.context.annotation.Configuration;
 /**
  * mybatis-plus 配置类
  * 用于设置拦截器
- * 分页拦截器 与 乐观锁拦截器
+ * 分页拦截器 & 乐观锁拦截器 & 阻止攻击内部拦截器
+ *  <a href="https://baomidou.com/pages/2976a3/#%E4%BD%BF%E7%94%A8%E6%96%B9%E5%BC%8F-%E4%BB%A5%E5%88%86%E9%A1%B5%E6">...</a>
  */
 @Configuration
 public class MPConfig {
@@ -19,8 +21,13 @@ public class MPConfig {
         // 分页拦截器
         mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor());
 
-        // 添加乐观锁拦截器
+        // 添加乐观锁拦截器 -> 当要更新一条记录的时候，希望这条记录没有被别人更新
+        // 仅支持 updateById(id) 与 update(entity, wrapper) 方法
+        // 在 update(entity, wrapper) 方法下, wrapper 不能复用!!!
         mybatisPlusInterceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+
+        // 防全表更新与删除插件 -> 针对 update 和 delete 语句 作用: 阻止恶意的全表更新删除
+        mybatisPlusInterceptor.addInnerInterceptor(new BlockAttackInnerInterceptor());
         return mybatisPlusInterceptor;
     }
 }
