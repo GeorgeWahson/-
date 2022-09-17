9ec0442ff90b9f1f294243354ee0556fdabfacd1
GeorgeWahson <georgewahson@qq.com>
2022-09-17 14:44:14 +0800

    测试类添加事务回滚

--- src/test/java/com/wahson/InfoSystemApplicationTests.java
+++ src/test/java/com/wahson/InfoSystemApplicationTests.java
@@ -5,15 +5,17 @@ import com.wahson.dao.BrandDao;
 import com.wahson.domain.Brand;
 import com.wahson.service.impl.BrandServiceImpl;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+
 @SpringBootTest
+@Transactional // 设置事务，打包及测试时，测试数据不会加入数据库
+@Rollback(value = true) // 默认回滚，但id还是会被占用
 class InfoSystemApplicationTests {
 
     @Autowired
